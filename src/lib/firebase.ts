6220d2fb4546157f140a23d84ac9cdf19c5069d1
てんでん <barururu3.pororo.kmt@gmail.com>
2024-04-15 13:58:06 +0900

    firebaseの設定を環境変数に

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,14 @@
 const { cert } = require("firebase-admin/app");
-const serviceAccount = require("../../image-comment-frame-firebase-adminsdk-m0gru-911cccd523.json");
+// const serviceAccount = require("../../image-comment-frame-firebase-adminsdk-m0gru-911cccd523.json");
 const admin = require("firebase-admin");
 if (admin.apps.length === 0) {
   admin.initializeApp({
-    credential: cert(serviceAccount),
+    credential: admin.credential.cert({
+      // cert()の中に直接JSON形式で代入
+      projectId: process.env.FSA_PROJECT_ID,
+      privateKey: process.env.FSA_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      clientEmail: process.env.FSA_CLIENT_EMAIL,
+    }),
     databaseURL: "https://image-comment-frame-default-rtdb.firebaseio.com",
   });
 }
