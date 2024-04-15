3edc824027f817e5c2aa03527ab78f2b31e14b94
てんでん <barururu3.pororo.kmt@gmail.com>
2024-04-15 14:00:48 +0900

    ビルドエラー修正

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ if (admin.apps.length === 0) {
     credential: admin.credential.cert({
       // cert()の中に直接JSON形式で代入
       projectId: process.env.FSA_PROJECT_ID,
-      privateKey: process.env.FSA_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: process.env.FSA_PRIVATE_KEY!.replace(/\\n/g, "\n"),
       clientEmail: process.env.FSA_CLIENT_EMAIL,
     }),
     databaseURL: "https://image-comment-frame-default-rtdb.firebaseio.com",
