543aeee7e24e2e1db12cc8787d032a9522b83fc7
てんでん <barururu3.pororo.kmt@gmail.com>
2024-04-20 23:43:52 +0900

    aboutページのリンクを追加

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,20 @@ export default function Top() {
         <img src="/hero.png" />
       </div>
       <div className="flex justify-center pt-4">
-        <Button variant={"custom"}>Register an NFT</Button>
+        <a
+          href="/register"
+          className="hover:text-indigo-200 transition duration-300"
+        >
+          <Button variant={"custom"}>Register an NFT</Button>
+        </a>
+
         <div className="px-4" />
-        <Button variant={"custom"}>Search registered NFT</Button>
+        <a
+          href="/list"
+          className="hover:text-indigo-200 transition duration-300"
+        >
+          <Button variant={"custom"}>Search registered NFT</Button>
+        </a>
       </div>
     </>
   );
