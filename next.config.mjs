7c548823d0d3e7ce22aab6203d18f3ff27475e88
てんでん <barururu3.pororo.kmt@gmail.com>
2024-04-21 00:11:28 +0900

    キャッシュコントロールしないように

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
