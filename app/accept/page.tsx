356c6694725a5a32f28e733369b3bdc3cf69b24d
てんでん <barururu3.pororo.kmt@gmail.com>
2024-05-16 17:36:07 +0900

    ビルドエラー修正

--- app/accept/page.tsx
+++ app/accept/page.tsx
@@ -2,9 +2,6 @@
 
 import { useCallback, useEffect, useState } from "react";
 
-interface Window {
-  onSignInSuccess?: (data: any) => void; // Replace 'any' with a more specific type if known
-}
 const Signin = () => {
   const [isClient, setIsClient] = useState(false);
   const [uuid, setUuid] = useState("");
